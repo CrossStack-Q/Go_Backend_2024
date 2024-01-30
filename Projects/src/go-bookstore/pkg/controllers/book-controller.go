b76d748f86e2d39c4de24b3f8beb88971a2dc3ff
Anurag Sharma <crossstack@gmail.com>
2024-01-30 23:51:03 +0530

    almost finished

--- Projects/src/go-bookstore/pkg/controllers/book-controller.go
+++ Projects/src/go-bookstore/pkg/controllers/book-controller.go
@@ -1,1 +1,47 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/CrossStack-Q/Go_Backend_2024/tree/main/Projects/src/go-bookstore/pkg/models"
+	"github.com/CrossStack-Q/Go_Backend_2024/tree/main/Projects/src/go-bookstore/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkgapplication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error in parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
