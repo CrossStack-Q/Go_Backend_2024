421e933db02103b2ffde9d4b36744806913c870c
CrossStack-Q <crossstack@gmail.com>
2024-02-29 23:08:26 +0530

    basic 1

---
+++ Projects/src/lenslocked/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting server on :3000")
+	http.ListenAndServe(":3000", nil)
+}
