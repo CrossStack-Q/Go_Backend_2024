e7bbe81d476e023deacc4ce286e319049d145a28
CrossStack-Q <CrossStack@gmail.com>
2024-03-14 23:35:13 +0530

    Backend Done

--- Projects/src/go-chat-react/backend/main.go
+++ Projects/src/go-chat-react/backend/main.go
@@ -1,8 +1,38 @@
 package main
 
-import "net/http"
+import (
+	"chat-app/websocket"
+	"log"
+	"net/http"
+)
+
+func serverWS(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		log.Println(err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
+
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+	log.Println("Started Server")
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serverWS(pool, w, r)
+	})
+
+}
 
 func main() {
-
+	setupRoutes()
 	http.ListenAndServe(":9000", nil)
 }
---
+++ Projects/src/go-chat-react/backend/websocket/pool.go
@@ -0,0 +1,49 @@
+package websocket
+
+import (
+	"fmt"
+)
+
+type Pool struct {
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}
+
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("Total connection pool = ", len(pool.Clients))
+			for k, _ := range pool.Clients {
+				fmt.Println(k)
+				k.Conn.WriteJSON(Message{Type: 1, Body: "New User added"})
+			}
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
+			fmt.Println("Total connection pool = ", len(pool.Clients))
+			for k, _ := range pool.Clients {
+				fmt.Println(k)
+				k.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected"})
+			}
+		case msg := <-pool.Broadcast:
+			fmt.Println("broadcasting a message")
+			for k, _ := range pool.Clients {
+				if err := k.Conn.WriteJSON(msg); err != nil {
+					fmt.Println("Error", err)
+				}
+			}
+		}
+	}
+}
